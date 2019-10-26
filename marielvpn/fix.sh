e32440bd25c6793eee4aa1c87b7d8c270cf9e9da
rayvynlee <53206160+rayvynlee@users.noreply.github.com>
2019-10-26 09:36:22 +0800

    Update fix.sh

--- marielvpn/fix.sh
+++ marielvpn/fix.sh
@@ -1,1 +1,14 @@
-.
+#!/bin/bash
+#MarielVPN GCloudFIX
+
+
+rm -rf /etc/openvpn/server.conf
+rm -rf /etc/openvpn/server443.conf
+
+wget -O /etc/openvpn/server.conf https://raw.githubusercontent.com/rayvynlee/bilat/master/marielvpn/server.conf
+wget -O /etc/openvpn/server443.conf https://raw.githubusercontent.com/rayvynlee/bilat/master/marielvpn/server443.conf
+
+chmod -R 755 /etc/openvpn/*
+echo "sysctl -p" >> /etc/rc.local
+
+reboot
