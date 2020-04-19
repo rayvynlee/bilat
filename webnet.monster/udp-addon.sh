fbaa2a0f84cc021faa79cef92af60c3ac27a5f18
rayvynlee <53206160+rayvynlee@users.noreply.github.com>
2020-04-20 07:20:05 +0800

    Update udp-addon.sh

--- webnet.monster/udp-addon.sh
+++ webnet.monster/udp-addon.sh
@@ -1,9 +1,6 @@
-rm -rf /etc/openvpn/server110.conf
+cat << EOF > /etc/openvpn/server111.conf
 
-
-cat << EOF > /etc/openvpn/server110.conf
-
-port 110
+port 111
 proto udp
 dev tun
 max-clients 100
@@ -24,12 +21,8 @@ keepalive 1 30
 persist-key 
 persist-tun
 verb 3
-status /var/www/html/status/tcp110.txt
+status /var/www/html/status/tcp111.txt
 #log-append /etc/openvpn/log/openvpn.log
 
 EOF
-
-/etc/init.d/openvpn restart
-
-rm -rf *.sh
 
